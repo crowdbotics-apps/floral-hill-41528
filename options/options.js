23fef3da9b05f5a1205c28c24f56b0e4a48aaa11
Crowdbotics <team@crowdbotics.com>
2023-06-16 03:58:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-hill-41528",
+  url: "https://floral-hill-41528.botics.co",
+  api: "https://floral-hill-41528.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
